3728adfbb0d445fb78da61e3c19b906bf38dce7a
Manali1892 <96368868+Manali1892@users.noreply.github.com>
2022-04-20 17:33:48 +0530

    Add files via upload

---
+++ src/Component/Progress.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+ function Progress(props){
+    return<>
+        
+   
+            <div className="card-body">
+              <h4 className="small font-weight-bold">{props.data.name} <span className="float-right">{props.data.value}%</span></h4>
+              <div className="progress mb-4">
+                <div className={`progress-bar ${props.data.color}`}
+                 role="progressbar" style={{width: `${props.data.value}%`}}
+                 value={`${props.data.value}`} 
+                  min="0" max="100"
+                   />
+              </div>
+            </div>
+          
+        
+    
+
+    
+    </>
+}
+export default Progress;
